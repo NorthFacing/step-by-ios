7221ba91bf6f1f14e5f11af304a806ab70ee10c6
Bob <0haizhu0@gmail.com>
2017-11-06 23:39:26 +0800

    第二个练习项目：checklists

---
+++ iOS-Apprentice/Checklists/Checklists/ChecklistViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Checklists
+//
+//  Created by Bob on 2017/11/6.
+//  Copyright © 2017年 Bob. All rights reserved.
+//
+
+import UIKit
+
+class ChecklistViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
